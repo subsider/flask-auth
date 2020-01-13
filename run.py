ea10513a47569c4da614136351989917455d1604
Carlos Cruz San Román <carlos.cruz@sngular.com>
2020-01-13 16:26:43 +0100

    Add database configuration

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 import resources
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+
+db = SQLAlchemy(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 api = Api(app)
 
 api.add_resource(resources.UserRegistration, '/registration')
